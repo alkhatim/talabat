a4596b693a03d2c1d18a7104108753b14973423c
alkhatim <mohammedalkhateem@gmail.co>
2021-02-24 17:33:09 +0400

    fix exchange function issue

--- helpers/exchange.js
+++ helpers/exchange.js
@@ -4,12 +4,16 @@ module.exports = (price, from, to, rate) => {
   switch (from) {
     case "USD":
       usd = parseFloat(price);
+      break;
     case "SDG":
       usd = parseFloat(price / rate.SDG);
+      break;
     case "AED":
       usd = parseFloat(price / rate.AED);
+      break;
     case "SAR":
       usd = parseFloat(price / rate.SAR);
+      break;
     default:
       usd = parseFloat(price);
   }
