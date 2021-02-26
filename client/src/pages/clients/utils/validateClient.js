60f59af400db8cc494e39ea783db1e709e469f4f
alkhatim <mohammedalkhateem@gmail.co>
2021-02-26 16:26:14 +0400

    max char length

--- client/src/pages/clients/utils/validateClient.js
+++ client/src/pages/clients/utils/validateClient.js
@@ -4,7 +4,7 @@ export default (client) => {
   const schema = Joi.object()
     .keys({
       _id: Joi.string().min(24).max(24).allow(""),
-      name: Joi.string().min(3).required(),
+      name: Joi.string().min(3).max(50).required(),
       phone: Joi.string().min(8).max(12).required(),
       phone2: Joi.string().min(8).max(12).allow(""),
       address: Joi.string().allow(""),
--- client/src/pages/users/utils/validateUser.js
+++ client/src/pages/users/utils/validateUser.js
@@ -4,7 +4,7 @@ export default (order) => {
   const schema = Joi.object()
     .keys({
       _id: Joi.string().min(24).max(24).allow(""),
-      username: Joi.string().min(3).max(30).required(),
+      username: Joi.string().min(3).max(50).required(),
       phone: Joi.string().min(8).max(12).required(),
       email: Joi.string().required(),
       role: Joi.string().valid("agent", "admin").required(),
